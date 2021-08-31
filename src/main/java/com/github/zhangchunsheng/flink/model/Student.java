ebf9a80e8400ba20b07c7d9512f6322dd1ff4944
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-08-31 11:05:27 +0800

    1. add mysql demo

---
+++ src/main/java/com/github/zhangchunsheng/flink/model/Student.java
@@ -0,0 +1,60 @@
+package com.github.zhangchunsheng.flink.model;
+
+public class Student {
+    public int studentId;
+    public String name;
+    public String password;
+    public int age;
+
+    public Student() {
+    }
+
+    public Student(int studentId, String name, String password, int age) {
+        this.studentId = studentId;
+        this.name = name;
+        this.password = password;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId=" + studentId +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
