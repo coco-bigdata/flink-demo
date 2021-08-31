8eafd9f3a1f798db55e8ae7ee1132e22af741c82
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-08-31 11:12:14 +0800

    1. add mysql demo

---
+++ src/main/java/com/github/zhangchunsheng/flink/TestMysqlJob.java
@@ -0,0 +1,19 @@
+package com.github.zhangchunsheng.flink;
+
+import com.github.zhangchunsheng.flink.source.SourceFromMySQL;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
+
+import java.util.Properties;
+
+public class TestMysqlJob {
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        env.addSource(new SourceFromMySQL()).print();
+
+        env.execute("Flink add data sourc");
+    }
+}
