d98973018cbaaea201002464cb736398a8591438
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-08-31 18:31:55 +0800

    1. add mysql demo

--- src/main/java/com/github/zhangchunsheng/flink/watermark/TumblingEventWindowExample.java
+++ src/main/java/com/github/zhangchunsheng/flink/watermark/TumblingEventWindowExample.java
@@ -5,10 +5,12 @@ import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.OutputTag;
 
 public class TumblingEventWindowExample {
     public static void main(String[] args) throws Exception {
@@ -75,6 +77,47 @@ public class TumblingEventWindowExample {
                     }
                 });
         resultStream.print();
+        env.execute();
+    }
+
+    public static void main2(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        env.setParallelism(1);
+        DataStream<String> socketStream = env.socketTextStream("localhost", 9999);
+        //保存被丢弃的数据
+        OutputTag<Tuple2<String, Long>> outputTag = new OutputTag<Tuple2<String, Long>>("late-data"){};
+        //注意，由于getSideOutput方法是SingleOutputStreamOperator子类中的特有方法，所以这里的类型，不能使用它的父类dataStream。
+        SingleOutputStreamOperator<Tuple2<String, Long>> resultStream = socketStream
+                // Time.seconds(3)有序的情况修改为0
+                .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<String>(Time.seconds(3)) {
+                    @Override
+                    public long extractTimestamp(String element) {
+                        long eventTime = Long.parseLong(element.split(" ")[0]);
+                        System.out.println(eventTime);
+                        return eventTime;
+                    }
+                })
+                .map(new MapFunction<String, Tuple2<String, Long>>() {
+                    @Override
+                    public Tuple2<String, Long> map(String value) throws Exception {
+                        return Tuple2.of(value.split(" ")[1], 1L);
+                    }
+                })
+                .keyBy(0)
+                .window(TumblingEventTimeWindows.of(Time.seconds(10)))
+                .sideOutputLateData(outputTag) // 收集延迟大于2s的数据
+                .allowedLateness(Time.seconds(2)) //允许2s延迟
+                .reduce(new ReduceFunction<Tuple2<String, Long>>() {
+                    @Override
+                    public Tuple2<String, Long> reduce(Tuple2<String, Long> value1, Tuple2<String, Long> value2) throws Exception {
+                        return new Tuple2<>(value1.f0, value1.f1 + value2.f1);
+                    }
+                });
+        resultStream.print();
+        //把迟到的数据暂时打印到控制台，实际中可以保存到其他存储介质中
+        DataStream<Tuple2<String, Long>> sideOutput = resultStream.getSideOutput(outputTag);
+        sideOutput.print();
         env.execute();
     }
 }
