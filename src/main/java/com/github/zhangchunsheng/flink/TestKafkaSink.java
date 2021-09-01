a30569c9f92c7ad80b84e7b6c1886b114e802032
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-09-01 14:04:23 +0800

    1. add elk demo

--- src/main/java/com/github/zhangchunsheng/flink/TestKafkaSink.java
+++ src/main/java/com/github/zhangchunsheng/flink/TestKafkaSink.java
@@ -1,13 +1,20 @@
 package com.github.zhangchunsheng.flink;
 
 import com.github.zhangchunsheng.flink.model.MetricEvent;
+import com.github.zhangchunsheng.flink.model.Student;
 import com.github.zhangchunsheng.flink.schemas.MetricSchema;
 import com.github.zhangchunsheng.flink.utils.ExecutionEnvUtil;
+import com.github.zhangchunsheng.flink.utils.GsonUtil;
 import com.github.zhangchunsheng.flink.utils.KafkaConfigUtil;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011;
+
+import java.util.Properties;
 
 public class TestKafkaSink {
     public static void main(String[] args) throws Exception{
@@ -23,5 +30,22 @@ public class TestKafkaSink {
                 .setParallelism(parameterTool.getInt("stream.sink.parallelism"));
 
         env.execute("flink learning connectors kafka");
+    }
+
+    public static void main1(String[] args) throws Exception{
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("zookeeper.connect", "localhost:2181");
+        props.put("group.id", "metric-group");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("auto.offset.reset", "latest");
+
+        SingleOutputStreamOperator<Student> student = env.addSource(new FlinkKafkaConsumer011<>(
+                "student",   //这个 kafka topic 需要和上面的工具类的 topic 一致
+                new SimpleStringSchema(),
+                props)).setParallelism(1)
+                .map(string -> GsonUtil.fromJson(string, Student.class)); //，解析字符串成 student 对象
     }
 }
