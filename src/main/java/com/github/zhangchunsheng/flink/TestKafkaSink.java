346c11f05d5fcafb44c7531088327d24ea122876
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-09-01 14:19:41 +0800

    1. add elk demo

--- src/main/java/com/github/zhangchunsheng/flink/TestKafkaSink.java
+++ src/main/java/com/github/zhangchunsheng/flink/TestKafkaSink.java
@@ -3,12 +3,13 @@ package com.github.zhangchunsheng.flink;
 import com.github.zhangchunsheng.flink.model.MetricEvent;
 import com.github.zhangchunsheng.flink.model.Student;
 import com.github.zhangchunsheng.flink.schemas.MetricSchema;
+import com.github.zhangchunsheng.flink.sink.SinkToMySQL1;
 import com.github.zhangchunsheng.flink.utils.ExecutionEnvUtil;
 import com.github.zhangchunsheng.flink.utils.GsonUtil;
 import com.github.zhangchunsheng.flink.utils.KafkaConfigUtil;
+import com.google.common.collect.Lists;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.java.utils.ParameterTool;
-import org.apache.flink.shaded.guava18.com.google.common.collect.Lists;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -54,16 +55,34 @@ public class TestKafkaSink {
                 new SimpleStringSchema(),
                 props)).setParallelism(1)
                 .map(string -> GsonUtil.fromJson(string, Student.class)); //，解析字符串成 student 对象
+    }
 
+    public static void main2(String[] args) throws Exception{
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("zookeeper.connect", "localhost:2181");
+        props.put("group.id", "metric-group");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("auto.offset.reset", "latest");
+
+        SingleOutputStreamOperator<Student> student = env.addSource(new FlinkKafkaConsumer011<>(
+                "student",   //这个 kafka topic 需要和上面的工具类的 topic 一致
+                new SimpleStringSchema(),
+                props)).setParallelism(1)
+                .map(string -> GsonUtil.fromJson(string, Student.class)); //
         student.timeWindowAll(Time.minutes(1)).apply(new AllWindowFunction<Student, List<Student>, TimeWindow>() {
             @Override
             public void apply(TimeWindow window, Iterable<Student> values, Collector<List<Student>> out) throws Exception {
-                ArrayList<Student> students = org.apache.flink.shaded.curator.org.apache.curator.shaded.com.google.common.collect.Lists.newArrayList(values);
+                ArrayList<Student> students = Lists.newArrayList(values);
                 if (students.size() > 0) {
                     System.out.println("1 分钟内收集到 student 的数据条数是：" + students.size());
                     out.collect(students);
                 }
             }
-        });
+        }).addSink(new SinkToMySQL1());
+
+        env.execute("flink learning connectors kafka");
     }
 }
