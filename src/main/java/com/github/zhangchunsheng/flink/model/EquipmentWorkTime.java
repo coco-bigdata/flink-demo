b95db3682e1aa9526c2252b15926e69de6164cbc
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-09-17 22:24:28 +0800

    1. add equipment status statistics

---
+++ src/main/java/com/github/zhangchunsheng/flink/model/EquipmentWorkTime.java
@@ -0,0 +1,10 @@
+package com.github.zhangchunsheng.flink.model;
+
+public class EquipmentWorkTime {
+    // timestamp statusDuration lastTimestamp
+    private Long startTimestamp;
+    private Long endTimestamp;
+    private Integer status;
+    private Integer statusDuration;
+    private Double durationMinute;
+}
