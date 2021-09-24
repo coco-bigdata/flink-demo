5e6f05099a0353ee668d05090ebc249540d9a719
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-09-24 14:42:05 +0800

    1. add day process

--- src/main/java/com/github/zhangchunsheng/flink/pomegranate/PaidWarningProcessFunction.java
+++ src/main/java/com/github/zhangchunsheng/flink/pomegranate/PaidWarningProcessFunction.java
@@ -1,5 +1,6 @@
 package com.github.zhangchunsheng.flink.pomegranate;
 
+import com.github.zhangchunsheng.flink.utils.DateUtil;
 import org.apache.flink.api.common.state.ValueState;
 import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.common.typeinfo.TypeHint;
@@ -43,5 +44,23 @@ public class PaidWarningProcessFunction extends KeyedProcessFunction<Integer, Pa
         current.setPaidCreateTime(paidWarn.getPaidCreateTime());
         paidWarnState.update(current);
         context.timerService().registerProcessingTimeTimer(current.getSystemTimestamp() + delay);
+    }
+
+    @Override
+    public void onTimer(long timestamp, OnTimerContext ctx, Collector<PaidWarn> out) throws Exception {
+        System.out.println("onTimer timestamp:" + DateUtil.format(timestamp, "yyyy-MM-dd HH:mm:ss"));
+        PaidWarn result = paidWarnState.value();
+        if(result != null && timestamp >= result.getSystemTimestamp() + delay && System.currentTimeMillis()- result.getPaidCreateTime() > 0) {
+            int snow = Integer.parseInt(DateUtil.format(System.currentTimeMillis(), "HH"));
+            if(snow > 7 && snow < 24) {
+                System.out.println("send");
+            }
+            out.collect(result);
+            ctx.timerService().registerProcessingTimeTimer(timestamp + delay);
+            result.setSystemTimestamp(timestamp);
+            paidWarnState.update(result);
+        } else {
+            System.out.println("no send");
+        }
     }
 }
